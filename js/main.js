ffff0f81fca63ea31b7b8b4463c627879c39fc40
vinosgray <komarichev@gmail.com>
2016-08-30 16:56:45 +0300

    courusel

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
 $(function() {
-    $('.jcarousel').jcarousel();
+    $('.jcarousel').jcarousel({
+        // Configuration goes here
+    });
+});
+$(function() {
+    $('.jcarousel').jcarousel({
+        // Core configuration goes here
+    });
+
+    $('.jcarousel-pagination').jcarouselPagination({
+        item: function(page) {
+            return '<a href="#' + page + '">' + page + '</a>';
+        }
+    });
 });
